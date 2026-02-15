544c3a811c2e78cf3fac8ac6c09a1aab9f667899
Wusteeeer <felicia04villarroel@gmail.com>
2026-02-15 10:44:30 +0100

    Fiender flyger med the force

--- Assets/Scripts/TheForce.cs
+++ Assets/Scripts/TheForce.cs
@@ -8,6 +8,8 @@ public class TheForce : MonoBehaviour
     private float shrinkTime;
     [Range(0.0f, 1.0f)]
     public float barSpeed;
+    public float SphereRadius;
+    public float heightForce;
 
     public void HandleButtonPress()
     {
@@ -15,6 +17,33 @@ public class TheForce : MonoBehaviour
             return;
         }
         hasPressedButton = true;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, SphereRadius);
+
+        GameObject nearestEnemy = hitColliders[0].gameObject;
+        float closestDistance = Vector3.Distance(transform.position, hitColliders[0].transform.position);
+
+        foreach (var collider in hitColliders) {
+            if (!collider.CompareTag("Enemy")) {
+                continue;
+            }
+            Debug.Log(collider.transform.name);
+            float currentDistance = Vector3.Distance(transform.position, collider.transform.position);
+
+            if (currentDistance < closestDistance) {
+                nearestEnemy = collider.gameObject;
+                closestDistance = currentDistance;
+            }
+            
+        }
+        
+        Rigidbody rb = nearestEnemy.GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            return;
+        }
+
+        rb.AddForce(Vector3.up * heightForce);
     }
 
     public void Update()
